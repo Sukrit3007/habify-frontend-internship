0b3066df1d48e71701c9380fdd1bfff5de12f892
Sukrit <sukrit3007@outlook.com>
2023-07-23 19:43:08 +0530

    adding cards and heading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card'
+import Heading from './components/Heading'
+
+
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Heading/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+
     </>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import Badge from 'react-bootstrap/Badge';
+
+function BasicExample() {
+  return (
+    <div>
+      <h3>
+         What are your best tips on how to deal with fear of abandoment? <Badge bg="secondary">New</Badge>
+      </h3>
+    </div>
+  );
+}
+
+export default BasicExample;
